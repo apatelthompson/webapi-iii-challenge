0161e3dcaa9cc520cbd5a01a50ae5dcf521f6d99
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-09-05 11:10:06 -0700

    made port dynmamic

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // code away!
+require("dotenv").config();
 const server = require("./server.js");
+const defaults = require("./config/defaults.js");
 
-const port = 5050;
-
-server.listen(port, () => {
-  console.log(`API running on port ${port}`);
+server.listen(defaults.port, () => {
+  console.log(
+    `\n*** Server Running on http://localhost:${defaults.port} ***\n`
+  );
 });
